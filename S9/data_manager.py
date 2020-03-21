33286fb8eda30226198fe19af6dafacd5bbf55d8
siva-sankar-a <sivashambhu@gmail.com>
2020-03-21 05:35:24 +0000

    Modifying data manager for new dataset

--- S9/data_manager.py
+++ S9/data_manager.py
@@ -8,6 +8,7 @@ class DataManager(object):
     """
     
     def __init__(self, batch_size=64, use_cuda=True, dataset_name='cifar10', 
+                 trainset = None, testset = None,
                  train_transforms=None, test_transforms=None):
         super().__init__()
 
@@ -30,9 +31,15 @@ class DataManager(object):
             else:
                 self.test_transforms = transforms.Compose([transforms.ToTensor(),
                                                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616))])
+            if trainset:
+                self.trainset = trainset
+            else: 
+                self.trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=self.train_transforms)
             
-            self.trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=self.train_transforms)
-            self.testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=self.test_transforms)
+            if testset:
+                self.testset = testset
+            else:
+                self.testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=self.test_transforms)
 
             # dataloader arguments - something you'll fetch these from cmdprmt
             dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=4, pin_memory=True) if use_cuda else dict(shuffle=True, batch_size=batch_size)
