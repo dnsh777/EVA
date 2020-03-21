49c1685dda16f25a4eda655d51018d94672fe419
siva-sankar-a <sivashambhu@gmail.com>
2020-03-21 06:47:16 +0000

    Data manager modification

--- S9/data_manager.py
+++ S9/data_manager.py
@@ -15,30 +15,29 @@ class DataManager(object):
         self.dataset_name = dataset_name
 
         if self.dataset_name == 'cifar10':
-            # Train Phase transformations
-            if train_transforms:
-                self.train_transforms = train_transforms
-            else:
-                self.train_transforms = transforms.Compose([transforms.RandomRotation(15),
-                                                transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
-                                                transforms.RandomHorizontalFlip(),
-                                                transforms.ToTensor(),
-                                                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616))])
-
-            # Test Phase transformations
-            if test_transforms:
-                self.test_transforms = test_transforms
-            else:
-                self.test_transforms = transforms.Compose([transforms.ToTensor(),
-                                                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616))])
             if trainset:
                 self.trainset = trainset
-            else: 
+            else:
+                # Train Phase transformations
+                if train_transforms:
+                    self.train_transforms = train_transforms
+                else:
+                    self.train_transforms = transforms.Compose([transforms.RandomRotation(15),
+                                                    transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
+                                                    transforms.RandomHorizontalFlip(),
+                                                    transforms.ToTensor(),
+                                                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616))])
                 self.trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=self.train_transforms)
             
             if testset:
                 self.testset = testset
             else:
+                # Test Phase transformations
+                if test_transforms:
+                    self.test_transforms = test_transforms
+                else:
+                    self.test_transforms = transforms.Compose([transforms.ToTensor(),
+                                                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616))])
                 self.testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=self.test_transforms)
 
             # dataloader arguments - something you'll fetch these from cmdprmt
