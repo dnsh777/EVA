07a2ce9f100d6d00a6ea9d68510901917b6f3d44
siva-sankar-a <sivashambhu@gmail.com>
2020-04-19 11:35:11 +0000

    Modifying utils

--- S12/utils.py
+++ S12/utils.py
@@ -15,3 +15,16 @@ def graphical_summary_cifar10(model, use_cuda=True, save=True):
         dot_graph.format = 'svg'
         dot_graph.render(f'model_architecture')
     return dot_graph
+
+def textual_summary_tiny_imagenet(model):
+    summary(model, input_size=(3, 64, 64))
+
+def graphical_summary_tiny_imagenet(model, use_cuda=True, save=True):
+    random_input = torch.randn(1, 3, 64, 64).cuda() if use_cuda else torch.randn(1, 3, 64, 64) 
+    model.eval()
+    y = model(Variable(random_input))
+    dot_graph = make_dot(y)
+    if save:
+        dot_graph.format = 'svg'
+        dot_graph.render(f'model_architecture')
+    return dot_graph
